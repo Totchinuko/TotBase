4db0c6f21956d75ffb6899bbae0e2b1410784ebf
Totchinuko <contact@lerieur.fr>
2021-03-05 09:07:35 +0100

    Rename that make more sense

--- Editor/EnforceTypePropertyDrawer.cs
+++ Editor/EnforceTypePropertyDrawer.cs
@@ -7,13 +7,12 @@ using UnityEditor;
 namespace TotBase
 {
     [CustomPropertyDrawer(typeof(EnforceTypeAttribute))]
-    public class PrettyListDrawer : PropertyDrawer
+    public class EnforceTypePropertyDrawer : PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EnforceTypeAttribute propAttribute = attribute as EnforceTypeAttribute;
             EditorGUI.BeginProperty(position, label, property);
-
             UnityEngine.Object obj = EditorGUI.ObjectField(position, property.objectReferenceValue, propAttribute.type, true);
             if (obj != null && propAttribute.type.IsAssignableFrom(obj.GetType()) && !EditorGUI.showMixedValue)
             {
