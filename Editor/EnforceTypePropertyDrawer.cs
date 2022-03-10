498dd8eb2e7fff35d4d124f2b64f47125e712480
Totchinuko <contact@lerieur.fr>
2022-03-10 17:45:32 +0100

    Fix type enforcer not allowing null.

--- Editor/EnforceTypePropertyDrawer.cs
+++ Editor/EnforceTypePropertyDrawer.cs
@@ -13,11 +13,13 @@ namespace TotBase
         {
             EnforceTypeAttribute propAttribute = attribute as EnforceTypeAttribute;
             EditorGUI.BeginProperty(position, label, property);
+            position = EditorGUI.PrefixLabel(position, new GUIContent(ObjectNames.NicifyVariableName(property.name)));
             UnityEngine.Object obj = EditorGUI.ObjectField(position, property.objectReferenceValue, propAttribute.type, false);
             if (obj != null && propAttribute.type.IsAssignableFrom(obj.GetType()) && !EditorGUI.showMixedValue)
             {
                 property.objectReferenceValue = obj;
-            }
+            } else if(obj == null)
+                property.objectReferenceValue = null;
             EditorGUI.EndProperty();
         }
     }
